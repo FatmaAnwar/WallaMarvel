a30940775c7ab1c0bd4c00c5732ebc8e03257485
FatmaAnwar <fatma.anwar.adqp@gmail.com>
2025-06-08 07:16:54 +0300

    Add search bar with filtering logic and layout adjustments
    - Integrated UISearchBar inside ListHeroesView
    - Connected search bar delegate to ListHeroesViewController
    - Added `searchHeroes(with:)` to ListHeroesPresenterProtocol
    - Implemented filtering logic based on hero name

--- WallaMarvel/List Heroes/ListHeroesPresenter.swift
+++ WallaMarvel/List Heroes/ListHeroesPresenter.swift
@@ -4,6 +4,7 @@ protocol ListHeroesPresenterProtocol: AnyObject {
     var ui: ListHeroesUI? { get set }
     func screenTitle() -> String
     func getHeroes()
+    func searchHeroes(with text: String)
 }
 
 protocol ListHeroesUI: AnyObject {
@@ -17,6 +18,7 @@ final class ListHeroesPresenter: ListHeroesPresenterProtocol {
     private var currentOffset = 0
     private var isLoading = false
     private var allHeroes: [CharacterDataModel] = []
+    private var filteredHeroes: [CharacterDataModel] = []
     
     init(getHeroesUseCase: GetHeroesUseCaseProtocol = GetHeroes()) {
         self.getHeroesUseCase = getHeroesUseCase
@@ -37,6 +39,15 @@ final class ListHeroesPresenter: ListHeroesPresenterProtocol {
             self.allHeroes += container.characters
             self.ui?.update(heroes: self.allHeroes)
             self.isLoading = false
+        }
+    }
+    
+    func searchHeroes(with text: String) {
+        if text.isEmpty {
+            ui?.update(heroes: allHeroes)
+        } else {
+            let result = allHeroes.filter { $0.name.lowercased().contains(text.lowercased()) }
+            ui?.update(heroes: result)
         }
     }
 }
