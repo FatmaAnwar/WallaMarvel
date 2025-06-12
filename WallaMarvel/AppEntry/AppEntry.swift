833ebfdae31ac232ba0998f2d6f0961d56247263
FatmaAnwar <fatma.anwar.adqp@gmail.com>
2025-06-12 13:04:08 +0300

    Cleaned up AppEntry by splitting logic into `configureInitialUI` and `configureImageCache`

--- WallaMarvel/AppEntry/AppEntry.swift
+++ WallaMarvel/AppEntry/AppEntry.swift
@@ -5,7 +5,6 @@
 //  Created by Fatma Anwar on 10/06/2025.
 //
 
-import Foundation
 import UIKit
 import Kingfisher
 
@@ -15,28 +14,34 @@ enum UIType {
 
 @available(iOS 15.0, *)
 final class AppEntry {
+    
+    private static let currentUIType: UIType = .swiftui
+    
     static func start(using windowScene: UIWindowScene) -> UIWindow {
         let window = UIWindow(windowScene: windowScene)
         let navigationController = UINavigationController()
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
-
-        let uiType: UIType = .swiftui
-
-        switch uiType {
+        
+        configureInitialUI(using: navigationController)
+        configureImageCache()
+        
+        return window
+    }
+    
+    private static func configureInitialUI(using navigationController: UINavigationController) {
+        switch currentUIType {
         case .uikit:
-            let coordinator = HeroesCoordinator(navigationController: navigationController)
-            coordinator.start()
+            HeroesCoordinator(navigationController: navigationController).start()
         case .swiftui:
-            let coordinator = SwiftUIHeroesCoordinator(navigationController: navigationController)
-            coordinator.start()
+            SwiftUIHeroesCoordinator(navigationController: navigationController).start()
         }
-
+    }
+    
+    private static func configureImageCache() {
         let cache = ImageCache.default
         cache.diskStorage.config.expiration = .days(7)
         cache.memoryStorage.config.totalCostLimit = 100 * 1024 * 1024
         cache.memoryStorage.config.countLimit = 100
-
-        return window
     }
 }
